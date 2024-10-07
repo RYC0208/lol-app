d0b5973de627a0f9975e596aadba87c756be5980
RYC0208 <ryc0208@naver.com>
2024-10-08 04:01:18 +0900

    try catch 제거

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -2,27 +2,22 @@ import { Rotation } from "@/types/ChampionRotation";
 import { Champion } from "../types/Champion";
 import { getChampionListAction } from "../server/ChampActions";
 export const getRotation = async (): Promise<Champion[]> => {
-  try {
-    const champions = await getChampionListAction();
+  const champions = await getChampionListAction();
 
-    const response = await fetch(`/api/rotation`);
+  const response = await fetch(`/api/rotation`);
 
-    if (!response.ok) {
-      throw new Error(`응답 에러: ${response.status}`);
-    }
-
-    const result: Rotation = await response.json();
-    const freeChampionIds = result.freeChampionIds;
-
-    const rotation: Champion[] = champions.filter((champion) =>
-      freeChampionIds.includes(Number(champion.key))
-    );
-    
-    return rotation;
-  } catch (error) {
-    console.error("데이터 fetching에러 :", error);
-    return [];
+  if (!response.ok) {
+    throw new Error(`응답 에러: ${response.status}`);
   }
+
+  const result: Rotation = await response.json();
+  const freeChampionIds = result.freeChampionIds;
+
+  const rotation: Champion[] = champions.filter((champion) =>
+    freeChampionIds.includes(Number(champion.key))
+  );
+
+  return rotation;
 };
 
 export const getLatestVersion = async () => {
